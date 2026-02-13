78f39009a6affe228f9b92eb96a2fd6eb26a0207
arthurrbp <arthurpadilha@icloud.com>
2026-02-12 22:56:25 -0300

    Atualiza estrutura do projeto

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <h1>Focooh</h1>
+      <p>Especialistas em Busdoor e Outdoor</p>
+    </section>
+  );
+}
+git commit -m "Meu primeiro commit"
